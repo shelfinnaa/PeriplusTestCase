553cf8892092fb4852544c1295239e622ca2b7d1
Shelfinna <77188043+shelfinnaa@users.noreply.github.com>
2025-04-30 09:12:30 +0700

    Set Up Configuration Test

--- src/main/java/com/example/base/DriverManager.java
+++ src/main/java/com/example/base/DriverManager.java
@@ -12,19 +12,12 @@ public class DriverManager {
     public static WebDriver getDriver() {
         if (driver == null) {
 
-        /*
-        Set the path to the ChromeDriver executable manually.
-        ChromeDriver version used: 135.0.7049.114 (to match the Chrome browser version on my Mac)
-        Downloaded from: https://googlechromelabs.github.io/chrome-for-testing/#stable
-        Note: Manual setup is used because automatic WebDriverManager configuration did not work previously.
-        */
-        System.setProperty("webdriver.chrome.driver", "/Users/shelfinna/Downloads/chromedriver-mac-arm64/chromedriver");
+        ChromeOptions options = new ChromeOptions();
 
         /*
         Set up Chrome options to make sure the browser and automation tool can work together smoothly. 
         Without this, the automated tests might not be able to open or control the browser properly.
         */
-        ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*"); 
         
         // Create the ChromeDriver 
